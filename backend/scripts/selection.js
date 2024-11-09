17ca14003a4863b3542b8ccbc01efa070cabf129
Ethan Wong <ewong25@student.ubc.ca>
2024-11-08 21:17:05 -0800

    Added a tokenizer function for the selection.js

--- backend/scripts/selection.js
+++ backend/scripts/selection.js
@@ -23,3 +23,46 @@ function setSelect(input, table) {
 
 }
 */
+function tokenizer(input_string){
+    // Trimming the string to deal with "   hi!"
+    const input_string_trim = input_string.trim();
+    const l_string = input_string_trim.length;
+    const tokens = [];
+    let curr = ""; 
+    // if we have " " it's a little different in SQL
+    let appostrophe_indicator = false;
+
+    for(var i = 0; i < l_string; i++){
+        if(!appostrophe_indicator){
+            if(input_string_trim[i] == '"'){
+                appostrophe_indicator = true;
+                curr = curr + input_string_trim[i];
+            }
+            else if(input_string_trim[i] != " "){
+                curr = curr + input_string_trim[i];
+            } else {
+                if(curr != ""){
+                    tokens.push(curr);
+                    curr = "";
+                }
+            }
+        } else {
+            if(input_string_trim[i] != '"'){
+                curr = curr + input_string_trim[i];
+            } else {
+                if(curr != ''){
+                    curr = curr + input_string_trim[i];
+                    tokens.push(curr);
+                    curr = "";
+                    appostrophe_indicator = false; 
+                }
+            }
+        }
+    }
+
+    return tokens;
+}
+
+//for testing purposes 
+let test_token = tokenizer('Input = "trash can" and why = "no one maybe"')
+console.log(test_token)
