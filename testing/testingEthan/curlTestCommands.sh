a3b01a865b71a5af01322e568cd69336168e162d
Ethan Wong <ewong25@student.ubc.ca>
2024-11-22 23:45:35 -0800

    Testing api

--- testing/testingEthan/curlTestCommands.sh
+++ testing/testingEthan/curlTestCommands.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-PORT=${1:-3000}
+PORT=$1
 BASE_URL="http://localhost:$PORT"
 
 # Test /projectFromPlace
 curl -G "$BASE_URL/projectFromPlace" --data-urlencode "Name,Address"
+curl -G "$BASE_URL/projectFromPlace" --data-urlencode "Name"
 
 # Test /getCuisinesAboveThreshold
 curl -G "$BASE_URL/getCuisinesAboveThreshold" --data-urlencode "threshold=2.0"
+curl -G "$BASE_URL/getCuisinesAboveThreshold" --data-urlencode "threshold=5.0"
 
 # Test /selectingPlace
 curl -G "$BASE_URL/selectingPlace" --data-urlencode "inputString=OpeningTime >= '09:00' AND ClosingTime <= '18:00'"
+curl -G "$BASE_URL/selectingPlace" --data-urlencode "name = stanley park"
